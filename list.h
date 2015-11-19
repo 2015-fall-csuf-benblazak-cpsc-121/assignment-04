50aeee214b62aa073f75bdace960fe5c65c393f7
Ubiquiphile <nerd_wizard@csu.fullerton.edu>
2015-11-19 10:18:02 -0800

    Update list.h

--- list.h
+++ list.h
@@ -1,5 +1,6 @@
 /* ----------------------------------------------------------------------------
  * Copyright &copy; 2015 Ben Blazak <bblazak@fullerton.edu>
+ * Copyright &copy; 2015 Ben Blazak <bblazak@fullerton.edu>
  * Released under the [MIT License] (http://opensource.org/licenses/MIT)
  * ------------------------------------------------------------------------- */
 
